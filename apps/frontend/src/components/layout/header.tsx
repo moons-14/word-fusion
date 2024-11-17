226ab0e92b3c6c574afcb81459b6418feb95bb89
moons-14 <moons@moons14.com>
2024-11-18 00:56:58 +0900

    header

---
+++ apps/frontend/src/components/layout/header.tsx
@@ -0,0 +1,32 @@
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+import { Settings, Twitter } from "lucide-react";
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+export const Header = () => {
+	return (
+		<header className="h-12 border-b border-border flex items-center justify-between p-4">
+			<h1 className="font-bold text-xl">
+				<Link href="/">Word Fusion</Link>
+			</h1>
+			<div className="flex items-center gap-2">
+				<Link href="/settings">
+					<Button variant="ghost" size="icon">
+						<Settings />
+					</Button>
+				</Link>
+				<a href="https://x.com/moons_dev" target="_blank" rel="noreferrer">
+					<Button variant="ghost" size="icon">
+						<Twitter />
+					</Button>
+				</a>
+			</div>
+		</header>
+	);
+};
